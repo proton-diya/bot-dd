07b677666e87b79e8400cd24a3918229cfbf1e99
proton-diya <gagofiti@proton.me>
2025-05-23 03:33:15 +0300

    Update clientMap.js

--- clientMap.js
+++ clientMap.js
@@ -2,6 +2,6 @@
 //  index.html?client=N
 var CLIENT_MAP = {
   "1pp": { folder: "1", file: "values.js" },
-  "2pp": { folder: "2", file: "values.js" },
+  "2pp": { folder: "2", file: "values.js" }
   
 };
