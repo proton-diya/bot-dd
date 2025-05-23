9046575684b0560c757e1f82ec8cc38658691644
proton-diya <gagofiti@proton.me>
2025-05-23 03:20:27 +0300

    Add files via upload

--- clientMap.js
+++ clientMap.js
@@ -1,7 +1,7 @@
 // clientMap.js
 //  index.html?client=N
 var CLIENT_MAP = {
-  "1": { folder: "1", file: "values.js" },
-  "2": { folder: "2", file: "values.js" },
+  "1": { folder: "./1", file: "values.js" },
+  "2": { folder: "./2", file: "values.js" },
   
 };
