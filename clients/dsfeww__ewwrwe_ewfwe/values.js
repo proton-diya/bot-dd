1d06013f29be5a56ae1830bdc8d73540b2a0bfaf
proton-diya <gagofiti@proton.me>
2025-05-28 18:42:35 +0300

    Добавлен dsfeww  ewwrwe ewfwe через Telegram

---
+++ clients/dsfeww__ewwrwe_ewfwe/values.js
@@ -0,0 +1,9 @@
+// values.js
+
+var fio        = "вацуаыва";
+var fio_en     = "dsfeww  ewwrwe ewfwe";
+var birth      = "32323.323.23";
+var rnokpp     = "32434";
+var email      = "23434";
+
+// (Добавьте сюда остальные переменные, если потребуется)
