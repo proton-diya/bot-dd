ad8b0a0498db84d0fdfeee40723b8b31fd5969d3
proton-diya <gagofiti@proton.me>
2025-05-28 19:23:16 +0300

    Update clientMap.js

--- clientMap.js
+++ clientMap.js
@@ -3,5 +3,5 @@
 var CLIENT_MAP = {
   "1pp": { folder: "1", file: "values.js" },
   "2pp": { folder: "2", file: "values.js" },
-  "dsfdf_dsfds_dsfsd": { folder: "dsfdf dsfds dsfsd", file: "values.js" }
+  "dsfdf_dsfds_dsfsd": { folder: "dsfdf_dsfds_dsfsd", file: "values.js" }
 };
